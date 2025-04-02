3064d50ece35fe7720d7054f1e3c6417875e7984
unknown <nebiatzemikael@gmail.com>
2025-04-02 03:42:15 -0700

    my function calculate mutiply_numbers

---
+++ functions/mutiply_numbers.py
@@ -0,0 +1,9 @@
+#this is the function that mutiply_numbers
+
+def mutiply_numbers(fir_num,sec_num):
+    mutiply_numbers=fir_num*sec_num
+    return mutiply_numbers
+#mutiply_numbers(fir_num,sec_num):
+mutiply = mutiply_numbers(8,9)
+print(mutiply)
+
