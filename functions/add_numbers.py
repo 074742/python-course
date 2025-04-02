c82b1df4cc2bfcbcf96a094874f83eebe3b4e2e0
unknown <nebiatzemikael@gmail.com>
2025-04-02 03:02:55 -0700

    a function that calculates sum of numbers

---
+++ functions/add_numbers.py
@@ -0,0 +1,8 @@
+# this is the function that add_numbers 
+    
+def add_numbers(fir_num,sec_num):
+    add_numbers=fir_num+sec_num
+    return add_numbers
+#add_numbers(fir_num,sec_num):
+Total = add_numbers(10,5)
+print(Total)
