1c9e6b2b1866e5e31b270ce979eb22afd0de51f9
unknown <nebiatzemikael@gmail.com>
2025-04-02 02:43:01 -0700

    my first function that calculates area of a triangle

---
+++ functions/calc_area.py
@@ -0,0 +1,10 @@
+"""this is a function that 
+calculates area of a triangle"""
+
+def calc_area(base, height):
+    calc_area = 1/2*base*height
+    return calc_area
+
+#calc_area (base,height):
+area = calc_area(12,6)
+print (area)
